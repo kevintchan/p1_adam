b5b95e252e82110d2730b8969c5fc65f60ce8c04
Kevin <kevintchan@berkeley.edu>
2012-03-25 13:09:37 -0700

    DelayNode

--- DelayNode.java
+++ DelayNode.java
@@ -1,27 +1,38 @@
-import java.util.LinkedList;
+import java.util.*;
+import java.lang.Math;
 
 public class DelayNode extends NetworkNode {
 
-    public int lambda;
-
-    public LinkedList queue<Packet>;
-
-    public DelayNode(int l, ...) {
-	this.lambda = l;
-	this.queue = new queue();
-    }
-
-    public int delayTime() {
-	return log(1-Math.random)/(-1*lambda);
-    }
-
-    
-    public void send(Packet p ) {
-        this.wait(delayTime());
-    }
-
-
-
-
-    
+  public int lambda;
+  
+  public Queue<Packet> queue;
+  
+  public DelayNode(int l, int inPort, int outPort) {
+    super(inPort, outPort);
+    this.lambda = l;
+    this.queue = new LinkedList<Packet>();
+    Thread t = new Thread() {
+        public void run() {
+          while (queue.peek() != null) {
+            long dTime = calculateDelayTime();
+            try {
+              sleep(dTime);
+            } catch (InterruptedException e) {
+              //do nothing
+            }
+            Packet p = queue.poll();
+            send(p);
+          }
+        }
+      };
+    t.start();
+  }
+  
+  public long calculateDelayTime() {
+    return (long) Math.log(1-Math.random())/(-1*lambda);
+  }
+  
+  public void handlePacket(Packet p) {
+    queue.offer(p);
+  }
 }
