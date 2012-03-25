c9c1a3639913bcb06a692f68061c7088ba7e5562
Adam Wang <adamwang@berkeley.edu>
2012-03-25 12:55:19 -0700

    Start DelayNode

--- DelayNode.java
+++ DelayNode.java
@@ -1,4 +1,27 @@
+import java.util.LinkedList;
+
 public class DelayNode extends NetworkNode {
 
+    public int lambda;
 
+    public LinkedList queue<Packet>;
+
+    public DelayNode(int l, ...) {
+	this.lambda = l;
+	this.queue = new queue();
+    }
+
+    public int delayTime() {
+	return log(1-Math.random)/(-1*lambda);
+    }
+
+    
+    public void send(Packet p ) {
+        this.wait(delayTime());
+    }
+
+
+
+
+    
 }
