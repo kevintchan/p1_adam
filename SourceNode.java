6e88bc8f559cb77e6256a205ca75742a5fd092c1
Adam Wang <adamwang@berkeley.edu>
2012-03-25 14:25:10 -0700

    SourceNodeUpdate

--- SourceNode.java
+++ SourceNode.java
@@ -1,9 +1,38 @@
 public class SourceNode extends NetworkNode {
 
-  public SourceNode(int inPort, int outPort) {
-    super(inPort, outPort);
-  }
-  
-  
+    public HashMap<int, long> startTime;
+    public long average;
+    public long parameter;
+    public int counter;
 
+
+    public SourceNode(int inPort, int outPort, long parameter) {
+	this.startTime = new HashMap<int, long>();
+	this.counter = 0;
+	this.average = 0;
+	this.parameter = parameter;
+	super(inPort, outPort);
+
+    }
+
+    public send(long startTime) {
+	packet p = new packet(IdCounter, false);
+	startTime.add(IdCounter, startTime);
+	super.send(p);
+	IdCounter++;
+    }
+    
+    public void handlePacket(Packet p) {
+	int id = p.getId();
+	long diff = gettimefrommain - StartTime.get(id);
+	if (diff =< 2*average) {
+	    average = (1 - parameter)*average - paramter*diff;
+	}
+    }
+
+    public
+	
+        
+  
+    
 }
